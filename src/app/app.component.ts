c65da89463a274a207b43856826d1243d4a9c177
Ankush Kumar Agarwal <ankush.agarwal@lendingkart.com>
2020-01-31 15:49:34 +0530

    Modified Css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todos-app';
+  title = 'Todos-App';
+  todo = [];
+
+  currentPriority: number = 0;
+
+  addTodos(newTodosLabel){
+    this.currentPriority++;
+    var newTodos = {
+      
+      Label: newTodosLabel,
+      Priority:this.currentPriority,
+
+    };
+    this.todo.push(newTodos);
+  }
+  
+  deleteTodos(todos){
+    this.todo=this.todo.filter( t=> t.Label != todos.Label );
+  }
 }
